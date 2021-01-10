cb3f9e8d54f5bfeb10ef485132f26deadb7fabd7
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-10 20:19:37 +0530

    ADDED: total of head and tail won

--- simulator.sh
+++ simulator.sh
@@ -1,13 +1,26 @@
 #! /bin/bash -x
 
-read -p " Please enter the choice \n 1.HEAD \n 2.TAIL " choice
+head=1
+tail=0
+count=0
+count1=0
+count2=0
 
-cointoss=$((RANDOM%2+1))
+while (( $count != 100 ))
+do
+	((count++))
+	cointoss=$((RANDOM%2))
 
-if [ $choice -eq $cointoss ]
-then
-	echo " head wins "
-else
-	echo " Tail wins "
-fi
-echo " The winner is $cointoss  "
+	if (( $cointoss == $head ))
+	then
+		((count1++))
+		echo " head wins "
+	elif (( $cointoss == $tail ))
+	then
+		((count2++))
+		echo " tail wins "
+	fi
+done
+
+echo $count1
+echo $count2
